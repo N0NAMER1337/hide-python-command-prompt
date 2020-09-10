7c4ef66372ff2b8e946fbaf481952e3b4f6e0466
N0NAMER1337 <paxa026@yandex.ru>
2020-09-10 19:15:17 +0300

    Bat File
    Bat File

---
+++ start.bat
@@ -0,0 +1,9 @@
+goto start
+--------------------
+Start the Python File
+--------------------
+:start
+@py python.py
+@msg * Python file has been started!
+@msg * By N0NAMER1337
+@msg * Donate: https://www.donationalerts.com/r/n0namer1337
